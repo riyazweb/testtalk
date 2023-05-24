d45656d066b30c6cd1095e64fb6b689ffe14192e
Mohammad Riyaz <116792240+riyazweb@users.noreply.github.com>
2023-05-24 12:43:01 +0530

    test

---
+++ src/utils/feautures.py
@@ -0,0 +1,19 @@
+import subprocess
+voice = 'en-IE-EmilyNeural'
+def speak(data):
+    voice = 'en-CA-LiamNeural'
+    filename = "data.mp3"
+
+    # Split the input text into chunks
+    chunks = data.split()
+    chunk_size = 1000
+    chunks = [chunks[i:i + chunk_size]
+              for i in range(0, len(chunks), chunk_size)]
+
+    # Convert and write each chunk to file
+    for chunk in chunks:
+        text = ' '.join(chunk)
+        command = f'edge-tts --voice "{voice}" --text "{text}"  --rate=+25%  --write-media "{filename}"'
+        subprocess.run(command, shell=True)
+
+    return True
